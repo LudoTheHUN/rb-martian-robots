cfa794cb7206e079c9d5da45e7d0aa9142c66f97
Ludwik G <ludoart@yahoo.com>
2020-01-17 20:54:25 +0000

    lein new project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to rb-martian-robots
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/rb_martian_robots/core_test.clj
@@ -0,0 +1,7 @@
+(ns rb-martian-robots.core-test
+  (:require [clojure.test :refer :all]
+            [rb-martian-robots.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
