5f7b230e47d44f19144fb1404331ca6aeb6bdab9
Ludwik G <ludoart@yahoo.com>
2020-01-18 23:31:03 +0000

    input file parsing into data

--- src/rb_martian_robots/core.clj
+++ src/rb_martian_robots/core.clj
@@ -1,4 +1,5 @@
 (ns rb-martian-robots.core
+  (:require [clojure.string :as str])
   (:gen-class))
 
 (defn foo
@@ -12,3 +13,51 @@
   [& args]
   (println "foo" args))
 
+
+; Lets read the input.robots file...
+(defn read-robots-file [filepath]
+  (slurp filepath))
+
+(def lines-per-robot 2)
+
+
+;5 3
+;1 1 E
+;RFRFRFRF
+;
+;3 2 N
+;FRRFLLFFRRFLL
+(defn empty-str? [x]
+  (= x ""))
+
+
+(defn read-x-y [line]
+  (vec (map #(Integer. %) (take 2 (str/split line #" ")))))
+
+
+(defn read-robot-lines
+  "Breaks the robot lines into per robot vector"
+  [robot-instruction-lines]
+  (filter #(= (count %) lines-per-robot)
+          (partition-by empty-str? robot-instruction-lines)))  ;;TODO fix for empty instructions
+
+
+(defn parse-robot-vector [robot-vector]
+  "Parse a single robot vector into a data representation"
+  {:position (read-x-y (first robot-vector))
+   :orientation (str (last (first robot-vector)))
+   :instructions (str/split (second robot-vector) #"")})
+
+(defn parse-input [s]
+  "prase the input string into a data representation"
+  (let [lines (str/split-lines s)
+        world-size (read-x-y (first lines))
+        per-robot-vectors (read-robot-lines (rest lines))]
+    {:world-size world-size
+     :robots (vec (map parse-robot-vector per-robot-vectors))}))
+
+
+(parse-input (read-robots-file "input.robots"))
+
+
+(read-robots-file "output.robots")
--- test/rb_martian_robots/core_test.clj
+++ test/rb_martian_robots/core_test.clj
@@ -2,6 +2,53 @@
   (:require [clojure.test :refer :all]
             [rb-martian-robots.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+
+(def input-string
+  "5 3\n1 1 E\nRFRFRFRF\n\n3 2 N\nFRRFLLFFRRFLL\n\n0 3 W\nLLFFFLFLFL")
+
+(def output-string
+  "1 1 E\n3 3 N LOST\n2 3 S")
+
+(def expected-parsed-input
+  {:world-size [5 3],
+   :robots [{:position [1 1], :orientation "E", :instructions ["R" "F" "R" "F" "R" "F" "R" "F"]}
+            {:position [3 2], :orientation "N", :instructions ["F" "R" "R" "F" "L" "L" "F" "F" "R" "R" "F" "L" "L"]}
+            {:position [0 3], :orientation "W", :instructions ["L" "L" "F" "F" "F" "L" "F" "L" "F" "L"]}]})
+
+(deftest parse-tests
+  (testing "that we can parse the input string"
+    (is (= expected-parsed-input
+           (parse-input input-string))))
+  (testing "minimal valid input works"
+        (is (= (parse-input "3 4")
+               {:world-size [3 4], :robots []})))
+  (testing "robots with no instruction input work"
+    ; TODO fix for robots with empty instructions.
+    (is (= (parse-input "3 4\n1 1 E\n")
+           {:world-size [3 4],
+            :robots [{:position [1 1] :orientation "E" :instructions []}]}))
+    (is (= (parse-input "3 4\n1 1 E\n\n\n5 5 S\nF")
+           {:world-size [3 4],
+            :robots [{:position [1 1] :orientation "E" :instructions []}
+                     {:position [5 5] :orientation "S" :instructions ["F"]}]}))))
+
+
+
+
+
+
+
+
+
+
+
+
+(use 'clojure.data)
+(diff expected-parsed-input
+   (parse-input input-string))
+
+
+
+
+
